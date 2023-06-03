7da2a0561b10f074b0d84ca7aadbe152d3840bcb
Ali-Khlifi <alikhlifi.ig2017@gmail.com>
2023-06-03 16:21:48 +0200

    Display a list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Hero} from "../moduls/hero";
+import {HEROES} from "../moduls/mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -11,6 +12,13 @@ export class HeroesComponent {
   hero : Hero = {
     id : 1,
     name : 'Ali'
+  }
+  heroes = HEROES;
+
+  selectedHero ?: Hero
+
+  onSelectHero(hero: Hero) {
+    this.selectedHero = hero;
   }
 }
 

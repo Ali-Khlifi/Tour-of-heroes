6437400cbd6309d2ed530c055a2710d2a7eaa24e
Ali-Khlifi <alikhlifi.ig2017@gmail.com>
2023-06-04 15:03:14 +0200

    Add router to navigate among components

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,35 @@
-import {Component, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from "../moduls/hero";
-
+import {ActivatedRoute} from "@angular/router";
+import { HeroService } from '../services/hero.service';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent {
-  @Input() hero ? : Hero  // to have this value from parent
 
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+  //@Input() hero ? : Hero  // to have this value from parent
+
+  ngOnInit() {
+    this.getHero();
+  }
+
+  hero: Hero | undefined;
+
+  getHero() {
+    const id  = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(
+      hero => { this.hero = hero; },
+    )
+  }
+  goBack() {
+    this.location.back();
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import {Hero} from "../moduls/hero";
 import { HeroService } from '../services/hero.service';
-import { MessageService } from '../services/message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  constructor(private heroService: HeroService,
-              private messageService: MessageService
-  ) { }
+  constructor(private heroService: HeroService) { }
   ngOnInit() {
     this.getHeros();
   }
